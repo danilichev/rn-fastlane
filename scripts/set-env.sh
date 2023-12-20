37ffb288f16a9684af24fb26a957edc5132d8fcf
danilichev <v.danilichev@gmail.com>
2023-12-20 22:52:01 +0200

    Change app icon based on environment

--- scripts/set-env.sh
+++ scripts/set-env.sh
@@ -7,6 +7,7 @@ ENV=$1
 
 ROOT_DIR="${PWD}"
 SECRETS_DIR="${ROOT_DIR}/secrets/${ENV}"
+APP_ICON="./src/assets/app-icon$(if [ "$ENV" = "production" ]; then echo ""; else echo "-$ENV"; fi).png"
 
 ENV_FILE=".env"
 
@@ -15,5 +16,6 @@ cp -f "${SECRETS_DIR}/${ENV_FILE}" "${ROOT_DIR}/${ENV_FILE}"
 npm run rnuc "${ENV_FILE}"
 npm run gradlew:clean
 npm run pod:install
+npm run appicon:create "${APP_ICON}"
 
 echo "Current environment is ${ENV}"
